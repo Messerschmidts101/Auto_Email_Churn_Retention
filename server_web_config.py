70a6398fba3ab0a738e257a8485234a1d70c493f
B. Ferrer <66350498+Messerschmidts101@users.noreply.github.com>
2025-07-19 02:30:51 +0800

    server config
    moved server constants to a config file

---
+++ server_web_config.py
@@ -0,0 +1,8 @@
+import os
+strPathStorageML = os.path.join(os.getcwd(),'website','Temporary_Files')
+strPathStorageLLM = os.path.join(os.getcwd(),'website','Temporary_Files','Context')
+strPathPersonaLLM = os.path.join(os.getcwd(),'website','Temporary_Files','persona_1.txt')
+strNameMLFinal = 'churn_prediction_model.pkl'
+strNameCSVTrain = "train.csv"
+strNameCSVScoring = "scoring.csv"
+strNameCSVScored = "scored.csv"
