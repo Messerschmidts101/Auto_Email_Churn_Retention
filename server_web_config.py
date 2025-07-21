508b0f8b19da9762c14a73746165ca0b9f06381e
B. Ferrer <66350498+Messerschmidts101@users.noreply.github.com>
2025-07-21 23:58:40 +0800

    done
    finished but can be improved

--- server_web_config.py
+++ server_web_config.py
@@ -1,8 +1,37 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+########################################################
+#######                                          #######
+#######                A. File Paths             #######
+#######                                          #######
+########################################################
 strPathStorageML = os.path.join(os.getcwd(),'website','Temporary_Files')
 strPathStorageLLM = os.path.join(os.getcwd(),'website','Temporary_Files','Context')
-strPathPersonaLLM = os.path.join(os.getcwd(),'website','Temporary_Files','persona_1.txt')
 strNameMLFinal = 'churn_prediction_model.pkl'
 strNameCSVTrain = "train.csv"
 strNameCSVScoring = "scoring.csv"
 strNameCSVScored = "scored.csv"
+strNameCSVEmails = "emails.csv"
+
+########################################################
+#######                                          #######
+#######                B. Emailing               #######
+#######                                          #######
+########################################################
+strEmailSubject = "Hello and We Miss You"
+strEmailFrom = os.getenv("EMAIL_APP_EMAIL")
+strEmailUser = os.getenv("EMAIL_APP_EMAIL") # yes intentionally same to strEmailFrom
+strEmailPass = os.getenv("EMAIL_APP_PASSWORD")
+
+########################################################
+#######                                          #######
+#######                  C. LLM                  #######
+#######                                          #######
+########################################################
+strAPILLM = os.getenv("GROQ_API_KEY")
+strPathPersonaLLM = os.path.join(os.getcwd(),'website','Temporary_Files','persona_1.txt')
+fltTemperature = 0.1
+intRetrieverK = 5
+intLLMAccessory = 4
