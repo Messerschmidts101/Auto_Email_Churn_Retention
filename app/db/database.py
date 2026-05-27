d39d28136da0c7aa14704fe250b7f2921507aa4d
B. Ferrer <66350498+Messerschmidts101@users.noreply.github.com>
2026-05-28 01:46:35 +0800

    undo database connection

--- app/db/database.py
+++ app/db/database.py
@@ -1,15 +1,9 @@
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from collections.abc import Generator
 
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, declarative_base, sessionmaker
-
-load_dotenv()
-
-DEFAULT_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/postgres"
-DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
-
+# Global and Shared
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/postgres"
 objEngine = create_engine(
     DATABASE_URL,
     pool_size=10,
